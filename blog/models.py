0a778aec011d4556b23e13e03d7eaa9978b53803
leah-jala <leahlafond@protonmail.com>
2022-12-06 12:30:01 +0000

    Altered field type in Post model

--- blog/models.py
+++ blog/models.py
@@ -2,38 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS =((0, "Draft"), (1, "Published"))
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Post(models.Model):
-
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
 
     class Meta:
         ordering = ['-created_on'] 
-        # The minus sign means to use descending order
 
     def __str__(self):
         return self.title
-    # returns a string representation of an object
 
     def number_of_likes(self):
         return self.likes.count()
-    # helper method to return the total number of likes on a post. 
-
 
 
 class Comment(models.Model):
-
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, 
+                             related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -42,7 +42,6 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['created_on']
-        # This will be in ascending order - oldest first
     
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
